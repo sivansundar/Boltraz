ba5fb73dc49ac84f810fd34e2bdd1c5aa08da1ea
Sivan <sivan.sundar@gmail.com>
2019-03-03 16:12:58 +0530

    Rebuilding UI. Implemented the BottomNavigationView as a part of the MDC.

--- app/src/main/java/com/boltraz/MainActivity.java
+++ app/src/main/java/com/boltraz/MainActivity.java
@@ -1,75 +1,51 @@
 package com.boltraz;
 
-import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Button;
+import android.view.MenuItem;
 import android.widget.FrameLayout;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.boltraz.Fragments.DashboardFragment;
-import com.boltraz.Fragments.MainFragment;
-import com.boltraz.Fragments.TimetableFragment;
-import com.boltraz.ListAdapters.ClassAnnouncementsListAdapter;
-import com.boltraz.Model.ClassAnnouncementsModel;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentChange;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.QuerySnapshot;
-import com.luseen.spacenavigation.SpaceItem;
-import com.luseen.spacenavigation.SpaceNavigationView;
-import com.luseen.spacenavigation.SpaceOnClickListener;
-import com.mikhaellopez.circularimageview.CircularImageView;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.annotation.Nullable;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+
+import com.boltraz.Fragments.DashboardFragment;
+import com.boltraz.Fragments.MainFragment;
+import com.boltraz.Fragments.NotesFragment;
+import com.boltraz.Fragments.TimetableFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity implements MainFragment.OnFragmentInteractionListener,
         DashboardFragment.OnFragmentInteractionListener,
-        TimetableFragment.OnFragmentInteractionListener{
+        TimetableFragment.OnFragmentInteractionListener,
+        NotesFragment.OnFragmentInteractionListener{
 
-    @BindView(R.id.space)
-    SpaceNavigationView space;
+
     @BindView(R.id.mainFrame)
     FrameLayout mainFrame;
+    @BindView(R.id.bottom_navigation)
+    BottomNavigationView bottomNavigation;
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
     private FirebaseFirestore mFirebaseFirestoredb;
 
 
-
-    private androidx.fragment.app.FragmentTransaction fragmentTransaction;
+    private FragmentTransaction fragmentTransaction;
 
     private MainFragment mainFragment;
     private DashboardFragment dashboardFragment;
     private TimetableFragment timetableFragment;
-
+    private NotesFragment notesFragment;
 
     String userID;
     private static final String TAG = "Boltraz MainActivity";
-
 
 
     @Override
@@ -81,59 +57,49 @@ public class MainActivity extends AppCompatActivity implements MainFragment.OnFr
         mainFragment = new MainFragment();
         dashboardFragment = new DashboardFragment();
         timetableFragment = new TimetableFragment();
+        notesFragment = new NotesFragment();
 
         setFragment(mainFragment);
 
-        space.initWithSaveInstanceState(savedInstanceState);
-        space.addSpaceItem(new SpaceItem("", R.drawable.round_home_black_36)); //0
-        space.addSpaceItem(new SpaceItem("", R.drawable.round_event_note_black_36));//1
-        space.addSpaceItem(new SpaceItem("", R.drawable.ic_launcher_background));//2
-        space.addSpaceItem(new SpaceItem("", R.drawable.round_account_circle_black_48));//3
-
-        space.setSpaceOnClickListener(new SpaceOnClickListener() {
+        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onCentreButtonClick() {
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.nav_home :
+                        setFragment(mainFragment);
+                        break;
 
-            }
+                    case R.id.nav_timetable :
+                        setFragment(timetableFragment);
+                        break;
 
-            @Override
-            public void onItemClick(int itemIndex, String itemName) {
-                if (itemIndex==0) {
-                    setFragment(mainFragment);
+                    case R.id.nav_notes :
+                        setFragment(notesFragment);
+                        break;
+
+                    case R.id.nav_dashboard :
+                        setFragment(dashboardFragment);
+                        break;
+
+                        default:
+                            return false;
                 }
 
-                if (itemIndex==1) {
-                    setFragment(timetableFragment);
-                }
-
-                if (itemIndex==3) {
-                    setFragment(dashboardFragment);
-                }
-
-
-                Toast.makeText((Context) MainActivity.this, itemIndex + "" + itemName, Toast.LENGTH_SHORT).show();
+                return true;
             }
 
-            @Override
-            public void onItemReselected(int itemIndex, String itemName) {
-
-            }
         });
-
 
 
     }
 
-    private void setFragment(androidx.fragment.app.Fragment fragment) {
+    private void setFragment(Fragment fragment) {
 
         FragmentTransaction fragmentTransaction1 = getSupportFragmentManager().beginTransaction().replace(R.id.mainFrame, fragment);
         fragmentTransaction1.commit();
 
 
     }
-
-
-
 
 
     @Override
@@ -150,7 +116,6 @@ public class MainActivity extends AppCompatActivity implements MainFragment.OnFr
 
 
     }
-
 
 
     @Override
