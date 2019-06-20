1dfdb6c7b96abbb255f5f118b1231b97c9935d78
sivan <sivan.sundar@gmail.com>
2019-06-20 20:57:01 +0530

    Working on the TodoList feature.

--- app/src/main/java/com/boltraz/ClassAnnouncementsActivity.java
+++ app/src/main/java/com/boltraz/ClassAnnouncementsActivity.java
@@ -1,22 +1,34 @@
 package com.boltraz;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.boltraz.Model.ClassAnnouncementsModel;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.chip.Chip;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.HashMap;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class ClassAnnouncementsActivity extends AppCompatActivity {
 
@@ -29,8 +41,11 @@ public class ClassAnnouncementsActivity extends AppCompatActivity {
     TextView descText;
     @BindView(R.id.author_chip)
     Chip authorChip;
+    @BindView(R.id.addAlert_fab)
+    FloatingActionButton FabaddAlert;
     private FirebaseDatabase mDatabase;
-    private DatabaseReference databaseReference;
+    private DatabaseReference databaseReference, todoReference;
+    String UID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,20 +56,23 @@ public class ClassAnnouncementsActivity extends AppCompatActivity {
         post_key = getIntent().getStringExtra("postID");
         mDatabase = FirebaseDatabase.getInstance();
         databaseReference = mDatabase.getReference().child("classAnnouncements");
-
+        todoReference = mDatabase.getReference().child("students");
         getPostDetails();
         Toast.makeText(this, "Post ID : " + post_key, Toast.LENGTH_SHORT).show();
+
+        UID = FirebaseAuth.getInstance().getUid();
+
     }
 
     private void getPostDetails() {
-        databaseReference.child("Class6B").child(post_key).addValueEventListener(new ValueEventListener() {
+        databaseReference.child("Class7A").child(post_key).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 ClassAnnouncementsModel model;
                 model = dataSnapshot.getValue(ClassAnnouncementsModel.class);
                 titleText.setText(model.getTitle());
                 descText.setText(model.getDesc());
-                authorChip.setText(model.getauthor()    );
+                authorChip.setText(model.getauthor());
 
 
             }
@@ -65,4 +83,58 @@ public class ClassAnnouncementsActivity extends AppCompatActivity {
             }
         });
     }
+
+    @OnClick(R.id.addAlert_fab)
+    public void onFabaddAlertClicked() {
+        //TODO: add click handling
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle("Add to your To-Do List?");
+        alertDialogBuilder.setMessage("Do you want to add '" + titleText.getText().toString() + "' to your To-Do List?");
+        alertDialogBuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //Nothing happens.
+            }
+        });
+        alertDialogBuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                String key = databaseReference.push().getKey();
+
+                String todo_title = titleText.getText().toString();
+                String todo_desc =  descText.getText().toString();
+                String todo_author = authorChip.getText().toString();
+
+                HashMap<String, Object> todoMap = new HashMap<>();
+                todoMap.put("title", todo_title);
+                todoMap.put("desc", todo_desc);
+                todoMap.put("author", todo_author);
+
+                todoReference.child("semester7").child(UID).child("todos").child(key).setValue(todoMap).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        View contextView = findViewById(R.id.mainLayout);
+
+                        Snackbar snackbar = Snackbar
+                                .make(contextView, "'" + todo_title + "' was successfully added to your To-Do List." , Snackbar.LENGTH_LONG);
+
+                        snackbar.show();
+                    }
+                })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.e("Announcement Activity", "onFailure: ", e);
+                            }
+                        });
+
+
+            }
+        });
+
+        alertDialogBuilder.show();
+    }
+
 }
